8596e95e0cff3c5e23764572cd31ea9e1fe8719e
Kehinde Aregbesola <adedimeji88@yahoo.com>
2023-05-19 14:05:00 +0100

    implemented the controller with the logics

---
+++ .net/Web/Multitracks.API/multitracks.API/Controllers/ArtistController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using multitracks.Core.Dtos;
+using multitracks.Core.Interfaces;
+
+namespace multitracks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+        private readonly IArtistService _artistService;
+        private readonly ILogger<ArtistController> _log;
+
+        public ArtistController(IArtistService artistService, ILogger<ArtistController> log)
+        {
+            _artistService = artistService;
+            _log = log;
+        }
+
+        [HttpGet("search/{name}", Name = "search")]
+        public async Task<IActionResult> Search(string name)
+        {
+            _log.LogInformation("Executing search artist endpoint");
+            var artist = await _artistService.SearchArtistByName(name);
+            return StatusCode(artist.StatusCode, artist);
+        }
+        [HttpPost("create-artist")]
+        public async Task<IActionResult> CreateArtist(CreateArtistDto request)
+        {
+            _log.LogInformation("Executing create artist endpoint");
+            var artist = await _artistService.CreateArtistAsync(request);
+            return StatusCode(artist.StatusCode, artist);
+        }
+    }
+}
---
+++ .net/Web/Multitracks.API/multitracks.API/Controllers/SongController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using multitracks.Core.Interfaces;
+
+namespace multitracks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private readonly ISongService _songService;
+        private readonly ILogger<SongController> _log;
+
+        public SongController(ISongService songService, ILogger<SongController> log)
+        {
+            _songService = songService;
+            _log = log;
+        }
+
+        [HttpGet, Route("list")]
+        public async Task<IActionResult> ListAllSong(int pageNumber, int pageSize)
+        {
+            _log.LogInformation("Executing list all songs endpoint");
+            var songs = await _songService.GetAllSongsAsync(pageNumber, pageSize);
+            return StatusCode(songs.StatusCode, songs);
+        }
+    }
+}
